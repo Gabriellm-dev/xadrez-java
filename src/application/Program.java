c508368dc9f272d544422703907113a0c3017e42
Gabriellm-dev <gabriellm.dev@gmail.com>
2023-08-01 14:47:29 -0300

    first class, boardgame, position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Sistema xadrez");
+        Position pos = new Position(3,5);
+        System.out.println(pos);
     }
 }
