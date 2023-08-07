6c9ce0718b7a310fca7817f9235a687e500377e0
Gabriellm-dev <gabriellm.dev@gmail.com>
2023-08-07 16:57:40 -0300

    check logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -38,8 +38,6 @@ public class UI {
         System.out.flush();
     }
 
-
-
     public static ChessPosition readChessPosition(Scanner sc){
         try {
             String s = sc.nextLine();
@@ -70,6 +68,10 @@ public class UI {
         System.out.println();
         System.out.println("Turn: " + chessMatch.getTurn());
         System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+
+        if (chessMatch.getCheck()){
+            System.out.println("CHECK!");
+        }
     }
 
     public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves){
